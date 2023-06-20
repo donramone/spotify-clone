23cb55faff7ecd1d4b4e6c99f28e5d2321f2eaec
Luigi <luigi_lipreri@hotmail.com>
2023-06-20 16:45:39 -0300

    terminado autenticacion modal y funcionalidad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
-import Sidebar from '@/components/Sidebar'
-import './globals.css'
-import { Figtree } from 'next/font/google'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen good music!',
-}
+  title: "Spotify Clone",
+  description: "Listen good music!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}><Sidebar>{children}</Sidebar> </body>
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,12 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { log } from "console";
+import { FaUserAlt } from "react-icons/fa";
+import {toast} from 'react-hot-toast'
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -12,15 +18,26 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const { onOpen } = useAuthModal();
   const router = useRouter();
-  const handleLogout = () => {
-    // Desp se implementa
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // Reset current song
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message)
+    } else {
+      toast.success('Logged out')
+    }
   };
   return (
     <div
       className={twMerge(
         `
-                h-fit
+        h-fit
                 bg-gradient-to-b
                 from-emerald-800
                 p-6
@@ -105,33 +122,50 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                     gap-x-4
                 "
         >
-          <>
-            <div>
-              <Button
-                onClick={() => {}}
-                className="
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button 
+                onClick={handleLogout} 
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button 
+                onClick={() => router.push('/account')} 
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          )  : (
+            <>
+              <div>
+                <Button
+                  onClick={onOpen}
+                  className="
                                 bg-transparent
                                 text-neutral-300
                                 font-medium
 
                             "
-              >
-                Sing up
-              </Button>
-            </div>
-            <div>
-              <Button
-                onClick={() => {}}
-                className="
+                >
+                  Sing up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={onOpen}
+                  className="
                                 bg-white
                                 px-6
                                 py-2
                             "
-              >
-                Login
-              </Button>
-            </div>
-          </>
+                >
+                  Login
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
